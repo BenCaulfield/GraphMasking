d644fe3f1dc69b3ba8eeb400448c0545b0e1c9a9
Wesley Miller <millew2@rpi.edu>
2013-05-12 19:39:38 -0300

    Update kSort.h
    Add functionality to store indices in the tree and return the indices in order of how the vectors were sorted.

--- kSort.h
+++ kSort.h
@@ -13,20 +13,22 @@ class kNode
 			left = NULL;
 			right = NULL;
 		}
-		kNode(std::vector<int> k_neighborhood, int val)
+		kNode(std::vector<int> k_neighborhood, int val, int num)
 		{
 			value = k_neighborhood;
 			diff = val;
 			mult = 1;
 			left = NULL;
 			right = NULL;
+			index = num;
 		}
-		void insert(std::vector<int> k_neighborhood, int new_diff)//inserts a k-neighborhood into a node or a child node
+		void insert(std::vector<int> k_neighborhood, int new_diff, int num)//inserts a k-neighborhood into a node or a child node
 		{
 			if(diff == -1)
 			{
 				value = k_neighborhood;
 				diff = 0;
+				index = num;
 				return;
 			}
 			
@@ -37,11 +39,11 @@ class kNode
 					//insert left
 					if(left == NULL)
 					{
-						left = new kNode(k_neighborhood, i);
+						left = new kNode(k_neighborhood, i, num);
 					}
 					else
 					{
-						left -> insert(k_neighborhood, i);
+						left -> insert(k_neighborhood, i, num);
 					}
 					return;
 				}
@@ -50,11 +52,11 @@ class kNode
 					//insert right
 					if(right == NULL)
 					{
-						right = new kNode(k_neighborhood, i);
+						right = new kNode(k_neighborhood, i, num);
 					}
 					else
 					{
-						right -> insert(k_neighborhood, i);
+						right -> insert(k_neighborhood, i, num);
 					}
 					return;					
 				}
@@ -85,6 +87,25 @@ class kNode
 			
 			return combine(leftv,value,rightv,mult);
 		}
+		std::vector<int> isort()//returns the sorted vector of k-neighborhoods
+		{
+			std::vector<int> leftv;
+			std::vector<int> rightv;
+			leftv.clear();
+			rightv.clear();
+			
+			if(left != NULL)
+			{
+				leftv = left -> isort();
+			}
+			
+			if(right != NULL)
+			{
+				rightv = right -> isort();
+			}
+			
+			return icombine(leftv,index,rightv);
+		}
 		std::vector<std::vector<int> > combine(std::vector<std::vector<int> > vec1, std::vector<int> middle, std::vector<std::vector<int> > vec2, int copies)
 		{
 			for(unsigned int i = 0; i < copies; i++)
@@ -98,10 +119,20 @@ class kNode
 	
 			return vec1;
 		}
+		std::vector<int> icombine(std::vector<int> vec1, int middle, std::vector<int> vec2)
+		{
+			vec1.push_back(middle);
+			for(unsigned int i = 0; i < vec2.size(); i++)
+			{
+				vec1.push_back(vec2[i]);
+			}
+	
+			return vec1;
+		}
 	private:
 		std::vector<int> value;//k-neighborhood vector held in node
+		int index;//index of k-neighborhood node
 		int diff;//index at which it differs from its parent node, 0 if head
-		int mult;
 		kNode* left;//pointer to left child
 		kNode* right;//pointer to right child
 };
